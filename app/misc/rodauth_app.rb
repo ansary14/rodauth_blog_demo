aa8d52fd41d2a2c6a7031f83c504d2ef14f22279
Ayub Ansary <ayubuap35@gmail.com>
2022-11-14 11:15:10 +0600

    Generate the file by rodauth installer which provides rodauth rails

---
+++ app/misc/rodauth_app.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class RodauthApp < Rodauth::Rails::App
+  # primary configuration
+  configure RodauthMain
+
+  # secondary configuration
+  # configure RodauthAdmin, :admin
+
+  route do |r|
+    rodauth.load_memory # autologin remembered users
+
+    r.rodauth # route rodauth requests
+
+    # ==> Authenticating requests
+    # Call `rodauth.require_account` for requests that you want to
+    # require authentication for. For example:
+    #
+    # # authenticate /dashboard/* and /account/* requests
+    # if r.path.start_with?("/dashboard") || r.path.start_with?("/account")
+    #   rodauth.require_account
+    # end
+
+    # require_authentication for posts
+    rodauth.require_authentication if r.path.start_with?('/posts')
+
+    # ==> Secondary configurations
+    # r.rodauth(:admin) # route admin rodauth requests
+  end
+end
