2f5a58f0d01fc932979ecdf7cc815a5c3e173582
Ayub Ansary <ayubuap35@gmail.com>
2022-11-15 20:50:57 +0600

    Associate posts to the accounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_08_121719) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_120223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -50,10 +50,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_08_121719) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "account_id", null: false
+    t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
   add_foreign_key "account_login_change_keys", "accounts", column: "id"
   add_foreign_key "account_password_reset_keys", "accounts", column: "id"
   add_foreign_key "account_remember_keys", "accounts", column: "id"
   add_foreign_key "account_verification_keys", "accounts", column: "id"
+  add_foreign_key "posts", "accounts"
 end
