66b9d5fc94bb5bc7e300000079d7cb1e07a1dad4
Ayub Ansary <ayubuap35@gmail.com>
2022-11-15 21:33:05 +0600

    Generate a new model Profile and associate with accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_15_120223) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_151321) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -54,9 +54,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_15_120223) do
     t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_profiles_on_account_id"
+  end
+
   add_foreign_key "account_login_change_keys", "accounts", column: "id"
   add_foreign_key "account_password_reset_keys", "accounts", column: "id"
   add_foreign_key "account_remember_keys", "accounts", column: "id"
   add_foreign_key "account_verification_keys", "accounts", column: "id"
   add_foreign_key "posts", "accounts"
+  add_foreign_key "profiles", "accounts"
 end
